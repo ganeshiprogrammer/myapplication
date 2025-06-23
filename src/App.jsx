ba3bab40006f8bf3aa55e0269f10c7ac13480267
ganeshiprogrammer <ganeshiprogrammer@gmail.com>
2025-06-23 11:45:11 +0530

    expense manager boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Expense from './components/Expense'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Hello This is ReactJS project</h1>
+      {/* <Expense></Expense> */}
+
+      <Expense/>
+    </div>
+  )
+}
+
+export default App
